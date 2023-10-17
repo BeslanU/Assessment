7a09221a027fa88a188058fa540f77f3ea60cc3e
92bes <92bes@DESKTOP-INI9Q7Q>
2023-10-16 21:26:34 -0400

    Add project files.

---
+++ Assessment/assessment.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestExample.Tests
+{
+    [TestFixture]
+    public class AddRemoveElementsTests
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        [Test]
+        public void AddNElementsAndAssert()
+        {
+            driver.Url = "https://the-internet.herokuapp.com/add_remove_elements/";
+            Thread.Sleep(1000);
+            int numberOfElementsToAdd = 5;
+            for (int i = 0; i < numberOfElementsToAdd; i++)
+            {
+                driver.FindElement(By.XPath("//button[@onclick='addElement()']")).Click();
+            }
+            int numberOfElements = driver.FindElements(By.ClassName("added-manually")).Count;
+            string message = $"Expected {numberOfElementsToAdd} elements, but found {numberOfElements} elements.";
+            Assert.That(numberOfElements, Is.EqualTo(numberOfElementsToAdd), message);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
